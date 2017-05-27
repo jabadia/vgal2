efbab80f00dbd579950c9139858a32c21460b917
Javier Abadía <javi.abadia@gmail.com>
2017-05-27 22:57:04 +0200

    cards are a component

---
+++ src/VideoCard.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div>
+    <div v-if="!video" class="ja-card">
+        <div class="ja-card-title">Ups!</div>
+        <div class="ja-card-content">
+            <span>no hay videos en este email</span>
+        </div>
+    </div>
+
+    <div v-if="video"
+         class="ja-card"
+         :class="{'ja-current': isCurrent}"
+    >
+        <!--<a :href="video.link">-->
+        <div class="ja-card-media">
+            <img :src="video.thumbnails.medium.url" alt="">
+        </div>
+        <div class="ja-card-content">
+            {{video.title}}
+        </div>
+        <!--</a>-->
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['video','isCurrent'],
+        data () {
+            return {}
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style lang="less" rel="stylesheet/less">
+
+    .ja-card {
+        opacity: 0.8;
+        img {
+            max-width: 100%;
+        }
+    }
+    .ja-current {
+        opacity: 1
+    }
+
+</style>
